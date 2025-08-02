ea3965a20782f80356935bbf0eba3f56e23d5715
추희도 <manner0814@sjhansol.sjeduhs.kr>
2025-08-02 12:26:51 +0900

    solved

---
+++ src/main/java/Problem/Q10757.java
@@ -0,0 +1,15 @@
+package Problem;
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+public class Q10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        System.out.println(A.add(B));
+    }
+}
