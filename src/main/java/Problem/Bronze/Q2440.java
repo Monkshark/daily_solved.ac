0dd1615a7f8487ba7fbe1669f202f634a6519de6
추희도 <manner0814@sjhansol.sjeduhs.kr>
2025-08-09 15:27:45 +0900

    solved

---
+++ src/main/java/Problem/Bronze/Q2440.java
@@ -0,0 +1,16 @@
+package Problem.Bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class Q2440 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine()) + 1;
+
+        while (N --> 0) {
+            for (int i = 0; i < N; i++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
