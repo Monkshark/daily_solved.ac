96f53a70c3703d833fd6cf754684caf1835c101a
추희도 <manner0814@sjhansol.sjeduhs.kr>
2025-08-11 13:10:34 +0900

    solved

---
+++ src/main/java/Problem/Bronze/Q9316.java
@@ -0,0 +1,12 @@
+package Problem.Bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class Q9316 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());
+        for (int i = 1; i <= N; i++) System.out.println("Hello World, Judge " + i + "!");
+    }
+}
