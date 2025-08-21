c5c158138373826b94d234386bace097f0a0acb6
추희도 <manner0814@sjhansol.sjeduhs.kr>
2025-08-21 11:38:55 +0900

    solved

--- src/main/java/Problem/Silver/Q26596.java
+++ src/main/java/Problem/Silver/Q26596.java
@@ -18,7 +18,7 @@ public class Q26596 {
         List<Integer> list = new ArrayList<>(s.values());
 
         for (int i = 0; i < list.size(); i++) {
-            for (int j = i; j < list.size(); j++) {
+            for (int j = i + 1; j < list.size(); j++) {
                 if (list.get(i) == Math.floor(list.get(j) * 1.618) || list.get(j) == Math.floor(list.get(i) * 1.618)) {
                     System.out.println("Delicious!");
                     return;
